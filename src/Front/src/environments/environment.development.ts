3ecba50393572504fe952f0dc37a0619b3c1e150
Florian <florian.drevet@epita.fr>
2026-03-19 19:06:30 +0100

    feat(environment): update MSAL configuration with specific authority and API scopes

--- src/Front/src/environments/environment.development.ts
+++ src/Front/src/environments/environment.development.ts
@@ -6,7 +6,8 @@ export const environment : EnvironmentInterface =
     api_url: "http://localhost:8080",
     msalConfig: {
       clientId: '24c34231-a984-43b3-8ac3-9278ebd067ef',
-      authority: 'https://login.microsoftonline.com/common',
+      authority: 'https://login.microsoftonline.com/cc625709-6696-4cf6-a330-7baf406f6a99',
       redirectUri: 'http://localhost:4200',
+      apiScopes: ['api://4f7f2dbd-8a11-42c4-bedc-acbb127e9394/Configuration.Write'],
     },
   };
