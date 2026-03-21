a5f74e6b694d17a9354cb03828b8840573ec9d7f
Florian <florian.drevet@epita.fr>
2026-03-21 16:29:15 +0100

    feat(config): refactor config detail page to use tabbed layout with improved empty states and update style budget

--- src/Front/src/app/features/config-detail/config-detail.component.ts
+++ src/Front/src/app/features/config-detail/config-detail.component.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslateModule } from '@ngx-translate/core';
 import {
@@ -32,6 +33,7 @@ const ROLES = ['Owner', 'Contributor', 'Reader'] as const;
     MatIconModule,
     MatProgressSpinnerModule,
     MatSelectModule,
+    MatTabsModule,
     MatTooltipModule,
   ],
   templateUrl: './config-detail.component.html',
