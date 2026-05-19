874b34d238a452a12a9c98dd34ee84b2d1f19c80
florian.drevet <florian.drevet@exakis-nelite.com>
2026-05-19 16:07:33 +0200

    migration

---
+++ src/Api/InfraFlowSculptor.Infrastructure/Migrations/20260519135917_Pat_Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfraFlowSculptor.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Pat_Repository : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectRepositories_ProjectId",
+                table: "ProjectRepositories",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfraConfigRepositories_InfrastructureConfigId",
+                table: "InfraConfigRepositories",
+                column: "InfrastructureConfigId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ProjectRepositories_ProjectId",
+                table: "ProjectRepositories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_InfraConfigRepositories_InfrastructureConfigId",
+                table: "InfraConfigRepositories");
+        }
+    }
+}
