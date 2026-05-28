84d829e579155505424ec596b591bb6190b23fb1
florian.drevet <florian.drevet@exakis-nelite.com>
2026-05-28 17:32:11 +0200

    refactor: remove unused components from imports in add-resource-dialog and config-detail-tags-section

--- src/Front/src/app/features/config-detail/sections/tags/config-detail-tags-section.component.ts
+++ src/Front/src/app/features/config-detail/sections/tags/config-detail-tags-section.component.ts
@@ -3,13 +3,13 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { TranslateModule } from '@ngx-translate/core';
 
-import { DsButtonComponent, DsIconButtonComponent, DsKeyValueInputComponent } from '../../../../shared/components/ds';
+import { DsButtonComponent, DsKeyValueInputComponent } from '../../../../shared/components/ds';
 import { ConfigDetailTagsSection } from './config-detail-tags-section.interface';
 
 @Component({
   selector: 'app-config-detail-tags-section',
   standalone: true,
-  imports: [DsButtonComponent, DsIconButtonComponent, DsKeyValueInputComponent, FormsModule, MatIconModule, TranslateModule],
+  imports: [DsButtonComponent, DsKeyValueInputComponent, FormsModule, MatIconModule, TranslateModule],
   templateUrl: './config-detail-tags-section.component.html',
   styleUrl: './config-detail-tags-section.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
